23dd7d9e270248f6059598929331f46abf2c0c84
dragonzeen <dragonzeen@gmail.com>
2017-05-17 14:00:08 +0900

    wallet 디자인 적용

--- client/main.js
+++ client/main.js
@@ -26,6 +26,60 @@ Template.wallets.helpers({
   }
 });
 
+
+
+Template.wallet.onCreated(function walletdetailOnCreated() {
+  var address = FlowRouter.getParam('walletAdderss');
+  Meteor.subscribe('mywallet', address);
+  Meteor.subscribe('price');
+});
+
+Template.wallet.rendered = function () {
+  if (!this.qrloaded) {
+    this.qrloaded = true;
+
+    var address = FlowRouter.getParam('walletAdderss');
+
+    var param = {
+      address: address
+    };
+
+    Meteor.call('getBalance', param, function (err, res) {
+      if (!err) {
+        console.log("getBalance:" + res);
+        Session.set(address + 'balance', res);
+      }
+    });
+
+    qrContents = "bitcoin:" + address;
+    var qrcodesvg = new Qrcodesvg(qrContents, "qrcode", 250, {
+      "ecclevel": 1
+    });
+    qrcodesvg.draw({
+      "method": "classic",
+      "fill-colors": ["#003658", "#0085CB", "#0085CB"]
+    }, {
+      "stroke-width": 1
+    });
+  }
+}
+
+Template.wallet.helpers({
+  walletaddress() {
+    return FlowRouter.getParam('walletAdderss');
+  },
+  mywallet() {
+    return Wallets.findOne();
+  },
+  mywalletbalance() {
+    var address = FlowRouter.getParam('walletAdderss');
+    return Session.get(address + 'balance');
+  },
+  price() {
+    return Price.findOne({_id:'btc_bithumb'}).price;
+  }
+});
+
 Template.walletlist.helpers({
   myWalletlist() {
     return Wallets.find();
