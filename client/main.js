be29bc975d21039fd300df0c279cf34c191ed4b2
dragonzeen <dragonzeen@gmail.com>
2017-05-17 15:10:02 +0900

    css 수정으로 데이터 안나오는 것 고침

--- client/main.js
+++ client/main.js
@@ -46,7 +46,6 @@ Template.wallet.rendered = function () {
 
     Meteor.call('getBalance', param, function (err, res) {
       if (!err) {
-        console.log("getBalance:" + res);
         Session.set(address + 'balance', res);
       }
     });
