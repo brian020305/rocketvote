52e2eedd0ee026294edcd4c5d0e110b175af889a
라오(김용진) <kimyongjin@coupang.com>
2017-05-18 16:03:06 +0900

    페이지 통합

--- client/main.js
+++ client/main.js
@@ -9,6 +9,7 @@ import './main.html';
 
 Wallets = new Mongo.Collection('wallets');
 Price = new Mongo.Collection('price');
+Candidates = new Mongo.Collection('candidates');
 client = new CoinStack('ab6d65b31aca913b0f8fece05c41a7', '623c31e6607785e1d237893226f6d7');
 
 
@@ -91,7 +92,6 @@ Template.login.events({
         var address = row._id;
 
         Session.set('myaddress', address);
-        Session.set('myKey', key);
         Session.set('isLogin', "true");
         FlowRouter.go('/candidates');
     }
@@ -176,10 +176,45 @@ Template.walletdetail.helpers({
   }
 });
 
-/*
-Template.walletdetail.events({
-  'click [name="wallet"]' (event, instance) {
-
-  },
+Template.candidates.onCreated(function candidatesOnCreated() {
+  Meteor.subscribe('candidates');
 });
-*/
+
+Template.candidate.onCreated(function candidateOnCreated() {
+  Meteor.subscribe('candidates');
+});
+
+Template.candidates.helpers({
+  candidateList() {
+    return Candidates.find();
+  }
+});
+
+Template.candidate.helpers({
+    candidate() {
+        var candidateId = FlowRouter.getParam('candidateId');
+        return Candidates.findOne(candidateId);
+  }
+});
+
+Template.candidate.events({
+    'click .vote': function(e) {
+        if(confirm("이 후보에게 투표할까용?")){
+            var candidateId = FlowRouter.getParam('candidateId');
+        var param = {
+            targetAddress: Candidates.findOne(candidateId).address,
+            pKey: Session.get("myKey"),
+            fromAddress: Session.get("myaddress")
+        }
+        Meteor.call('vote', param, function(err, res) {
+            if(!err) {
+                var voteresult=res;
+                console.log(voteresult);
+                alert("이미 투표 하셨네용ㅎ");
+            }
+        });
+        } else {
+            alert("신중히 투표하세요.");
+        }
+    }
+});
